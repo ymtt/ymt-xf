bfc90b067437543f794df35c323e28dffba607d9
pzhihao <864300472@qq.com>
2015-05-11 18:10:38 +0800

    添加消息弹出插件：nl.x-services.plugins.toast 移动cordova.js位置到上一级目录 form.html:修改alert提示 index1.html没有homelogo值则返回welcome.html shezhi.html皮肤切换 welcome.html：更改为程序入口 zixun.html:动态获取返回

---
+++ assets/www/cordova_plugins.js
@@ -0,0 +1,21 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/nl.x-services.plugins.toast/www/Toast.js",
+        "id": "nl.x-services.plugins.toast.Toast",
+        "clobbers": [
+            "window.plugins.toast"
+        ]
+    },
+    {
+        "file": "plugins/nl.x-services.plugins.toast/test/tests.js",
+        "id": "nl.x-services.plugins.toast.tests"
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "nl.x-services.plugins.toast": "2.0.4"
+}
+// BOTTOM OF METADATA
+});
--- assets/www/js/cordova_plugins.js
+++ assets/www/js/cordova_plugins.js
@@ -1,5 +1,13 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
-module.exports = [];
+module.exports = [
+    {
+            "file": "plugins/com.ionic.keyboard/www/keyboard.js",
+            "id": "com.ionic.keyboard.keyboard",
+            "clobbers": [
+                "cordova.plugins.Keyboard"
+            ]
+    }
+];
 module.exports.metadata = 
 // TOP OF METADATA
 {}
