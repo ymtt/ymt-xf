b6f60959c5672235a2fedffbc8dcd5fef50606eb
zl <769952397@qq.com>
2015-05-23 10:47:54 +0800

    论坛 报考指南

--- assets/www/js/forum.js
+++ assets/www/js/forum.js
@@ -18,3 +18,22 @@ function  change_nav(heath_center){
     }
     }
     }
+
+function h1(x) {
+    document.getElementsByClassName("div8")[x].style.backgroundImage = "url(image/forwarding_Press-the_icon.png)";
+}
+function h2(x){
+    document.getElementsByClassName("div8")[x].style.backgroundImage= "";
+}
+function a1(x) {
+    document.getElementsByClassName("div9")[x].style.backgroundImage = "url(image/comment_collect1_icon.png)";
+}
+function a2(x){
+    document.getElementsByClassName("div9")[x].style.backgroundImage= "";
+}
+function s1(x) {
+    document.getElementsByClassName("div10")[x].style.backgroundImage = "url(image/comment_comment1_icon.png)";
+}
+function s2(x){
+    document.getElementsByClassName("div10")[x].style.backgroundImage= "";
+}
---
+++ assets/www/js/kaoshishichang.js
@@ -0,0 +1,52 @@
+/**
+ * Created by Administrator on 2015/5/21.
+ */
+function a()
+{
+
+    var oBtn = document.getElementById("btn1");
+    var oDemo = document.getElementById("demo1");
+
+    if (oDemo.style.display == "block") {
+        oBtn.style.background = "url(image/the-arrow_icon_right.png)no-repeat 80% 50%"
+        oDemo.style.display = "none";
+    }
+    else {
+        oBtn.style.background = "url(image/the-arrow_icon.png)no-repeat 80% 50%"
+        oDemo.style.display = "block";
+    }
+
+
+}
+function b()
+{
+
+    var oBtn1 = document.getElementById("btn2");
+    var oDemo1 = document.getElementById("demo2");
+
+    if (oDemo1.style.display == "block") {
+        oBtn1.style.background = "url(image/the-arrow_icon_right.png)no-repeat 80% 50%"
+        oDemo1.style.display = "none";
+    }
+    else {
+        oBtn1.style.background = "url(image/the-arrow_icon.png)no-repeat 80% 50%"
+        oDemo1.style.display = "block";
+    }
+
+}
+function c()
+{
+
+    var oBtn2 = document.getElementById("btn3");
+    var oDemo2 = document.getElementById("demo3");
+
+    if (oDemo2.style.display == "block") {
+        oBtn2.style.background = "url(image/the-arrow_icon_right.png)no-repeat 80% 50%"
+        oDemo2.style.display = "none";
+    }
+    else {
+        oBtn2.style.background = "url(image/the-arrow_icon.png)no-repeat 80% 50%"
+        oDemo2.style.display = "block";
+    }
+
+}
