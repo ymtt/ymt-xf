88d0ae1df615f46f0c821ff15abd34204a34c5db
pzhihao <864300472@qq.com>
2015-06-02 17:40:21 +0800

    资讯首页，文字新闻

--- assets/www/js/getnews.js
+++ assets/www/js/getnews.js
@@ -1,3 +1,4 @@
+//公告详情，教育资讯的ajax
 function getnews(type,func,start,end,wh){
             var session=window.sessionStorage.getItem("session");
             var url='http://120.24.172.105:8000/fw?controller=com.xfsm.action.ArticleAction';
@@ -16,6 +17,7 @@ function getnews(type,func,start,end,wh){
                             var list=json.datas['listData'];
                             $.each(list,function(key){
                                 var title=list[key]['article_title'];
+                                var content=list[key]['content'];
                                 var hitnum=list[key]['hitnum'];
                                 var id=list[key]['id'];
 
@@ -89,3 +91,62 @@ function CreateNoticeDom(title,content,id){
             "</p></div></a></div>"
   );
  }
+
+ //热点资讯,zixuntype:new表示最新资讯，read表示热点资讯
+ function hotzixun(type,zixuntype){
+         var session=window.sessionStorage.getItem("session");
+         var url='http://120.24.172.105:8000/fw?controller=com.xfsm.action.ArticleAction';
+         if(session==null){
+            alert("登录后进行查看");
+         }else{
+            $.ajax({
+                type:'get',
+                dataType:'json',
+                url:url,
+                data:{"SESSIONID":session,"m":"list","type":type,"order":zixuntype,"start":"1","pagesize":"1"},
+                success:function(data){
+                    $(".loading").remove();
+                    var list=data.datas['listData'];
+                    $.each(list,function(key){
+                        var title=list[key]['article_title'];
+                        var content=list[key]['content'];
+                        var pl=list[key]['hitnum'];
+                        var id=list[key]['id'];
+                        var obj=JSON.stringify(list[key]);
+                        if(key==0){
+                            if(zixuntype=="read"){
+                                CreateHotZixun(title,content,id);
+                                //将id和对象转换成的字符串对应，并存储到localstorage
+                                window.localStorage.setItem(id,obj);
+                            }else if(zixuntype=="new"){
+                                CreateNewZixun(title,content,pl,id);
+                                window.localStorage.setItem(id,obj);
+                            }
+                        }else{
+                            return false;
+                        }
+                    });
+                },error:function(error){
+                    alert("错误！");
+                }
+            });
+         }
+ }
+ function CreateHotZixun(title,content,id){
+       $("#nav_tab1").append("<div class='div6'><div class='div7'><p>热点资讯</p></div><span class='span-1'><a href='shipinzixun.html'><p>更多</p></a></span></div><div class='div10'><a href='javascript:Towenzizixun("+"\""+id+"\""+",\"wenzizixunXQ.html\""+")'><div class='div11'><p>"+title+
+       "</p></div><div class='div12'><p>"+content+"</p></div><div class='div13'></div></a></div>");
+        //alert(title+content);
+ }
+ function CreateNewZixun(title,content,pl,id){
+      $("#nav_tab1").append("<div class='div6'><div class='div7'><p>最新咨询</p></div><span class='span-1'><a href='wenzizixun.html'><p>更多</p></a></span></div><div class='div14'><a href='javascript:Towenzizixun("+"\""+id+"\""+",\"wenzizixunXQ.html\""+")'><div class='div15'><p>"+title+
+      "</p></div><div class='div16'><p>"+content+
+      "</p></div><div class='div17'><div class='div18'><p>"+pl+
+      "</p></div><div class='div19'><img src='image/comment_comment_icon.png'/></div></div></a></div>");
+ }
+
+function Towenzizixun(id,url){
+    //存储点击之后的id，在详情页取出并使用他来在localstorage根据id取出对象字符串
+    window.localStorage.setItem("wenziid",id);
+    window.location.href=url;
+    //alert(JSON.stringify(o));
+}
