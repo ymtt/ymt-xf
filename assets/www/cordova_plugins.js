01bc147c6be252f5b7244741789d2ca733d32b09
pzhihao <864300472@qq.com>
2015-06-08 15:14:23 +0800

    推荐给好友(分享)- cordova plugin add nl.x-services.plugins.socialsharing 仅安卓(IOS需要安装Social.framework和MessageUI.framework插件)

--- assets/www/cordova_plugins.js
+++ assets/www/cordova_plugins.js
@@ -10,12 +10,20 @@ module.exports = [
     {
         "file": "plugins/nl.x-services.plugins.toast/test/tests.js",
         "id": "nl.x-services.plugins.toast.tests"
+    },
+    {
+        "file": "plugins/nl.x-services.plugins.socialsharing/www/SocialSharing.js",
+        "id": "nl.x-services.plugins.socialsharing.SocialSharing",
+        "clobbers": [
+            "window.plugins.socialsharing"
+        ]
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-    "nl.x-services.plugins.toast": "2.0.4"
+    "nl.x-services.plugins.toast": "2.0.4",
+    "nl.x-services.plugins.socialsharing": "4.3.18"
 }
 // BOTTOM OF METADATA
 });
